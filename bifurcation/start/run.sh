f675e9d830a5484178adcfd5d400b7c64f97ef75
Samuel Zapolsky <samzapo@gmail.com>
2016-01-28 12:49:06 -0500

    diverging behavior test works

---
+++ bifurcation/start/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+pushd .
+cd ${DIR}
+
+# clean up worksapce
+#git clean -dfx ../start ../sample
+
+# re-parse '.in' files
+#../../setup-tests.sh ../start/*.in ../sample/*.in
+
+${PACER_INTERFACE_PATH}/pacer-driver --stepsize 0.000000000001 $@
+
+popd
---
+++ bifurcation/start/run_all_tests.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+pushd .
+cd ${DIR}
+
+# clean up worksapce
+git clean -dfx ../start ../sample*
+
+# re-parse '.in' files
+../../setup-tests.sh ../start/*.in ../sample*/*.in
+
+for i in $@
+do
+  sed -i.bak "s#>.*</task-directory>#>$i</task-directory>#g" plugins.xml
+  screen -d -m ${PACER_INTERFACE_PATH}/pacer-driver --stepsize 0.000000000001
+  sleep 10s
+done
+
+popd
