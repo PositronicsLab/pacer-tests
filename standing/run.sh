356b36c1bd0352f10f5908e2ec6a09da98e7c419
Samuel Zapolsky <samzapo@gmail.com>
2016-03-13 23:46:24 -0400

    updated pitch slightly, inserting manipulator example without altering existing experiments

--- standing/run.sh
+++ standing/run.sh
@@ -5,7 +5,5 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
   # re-parse '.in' files
   ../setup-tests.sh *.in
 
-  moby-driver -p=${PACER_BINARY_PATH}/Plugin/Component/inverse-dynamics/libTestPlugin.so -s=0.001 -r -mt=3 -vcp model.xml
-
-
+  moby-driver -p=${PACER_SIMULATOR_PATH}/libPacerMobyPlugin.so -r -s=0.001 -oi -mt=10 model.xml > out.log
 
