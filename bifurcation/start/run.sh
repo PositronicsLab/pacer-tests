e1d83b00f1f27c3cab0f6038dd8c6353471f66ea
Samuel Zapolsky <samzapo@gmail.com>
2016-05-19 10:32:43 -0400

    updated bifuraction tests

--- bifurcation/start/run.sh
+++ bifurcation/start/run.sh
@@ -3,12 +3,14 @@ DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 pushd .
 cd ${DIR}
 
+source ${BUILD_PATH}/Pacer/setup.sh
+
 # clean up worksapce
 git clean -dfx ../start ../sample*
 
 # re-parse '.in' files
 ../../setup-tests.sh ../start/*.in ../sample*/*.in
 
-moby-driver -p=${PACER_SIMULATOR_PATH}/libPacerMobyPlugin.so -s=0.001 -r -oi model.xml
+lldb -- moby-driver -p=${PACER_SIMULATOR_PATH}/libPacerMobyPlugin.so -s=0.001 -r avg-model/model.xml
 
 popd
