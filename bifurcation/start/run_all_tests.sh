e2dd6d30e9b91ccaef232cc00c8294bdeb3828f0
Samuel Zapolsky <samzapo@gmail.com>
2016-05-26 16:41:33 -0400

    updated gait pattern

--- bifurcation/start/run_all_tests.sh
+++ bifurcation/start/run_all_tests.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
-DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
-pushd .
-cd ${DIR}
-
+source ${BUILD_PATH}/Pacer/setup.sh
 # clean up worksapce
-git clean -dfx ../start
+git clean -dfx .
 
 # re-parse '.in' files
-${PACER_SCRIPTS_PATH}/setup-tests.sh ../*.in
+${PACER_SCRIPT_PATH}/setup-tests.sh *.in
 
 for i in $@
 do
   sed -i.bak "s#>.*</task-directory>#>$i</task-directory>#g" plugins.xml
-  ./run.sh $1  
+  screen -d -m -L ./run.sh $1  
   sleep 10s
 done
-
-popd
