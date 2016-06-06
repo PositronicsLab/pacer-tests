5f8aac4be9f5898bdf93ea65a1df729a3328353a
Za-Za <bradcanaday@gwmail.gwu.edu>
2016-06-06 16:11:54 -0400

    a

---
+++ trotting/place-robot.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+NAME=`basename $1 .xml`
+cat ${DIR}/sim-ground-model.xml > $NAME-sim.xml
+
+sed -i.bak 's#<XML >##g' $1
+sed -i.bak 's#<Moby >##g' $1
+sed -i.bak 's#</Moby>#</MOBY>#g' $1
+
+cat $1 >> $NAME-sim.xml
+
+cat $NAME-sim.xml > model.xml
---
+++ trotting/use-model.sh
@@ -0,0 +1,2 @@
+./place-robot.sh $1
+${PACER_COMPONENT_PATH}/monte-carlo-simulation/sample.bin --no-pipe --duration 2 --stepsize 0.01 --sample 1 --control \
