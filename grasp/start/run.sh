61c981c2c16f449c54147409e76cb1c4a290ebf1
Samuel Zapolsky <samzapo@gmail.com>
2016-07-27 19:45:38 -0400

    setup grasping example

---
+++ grasp/start/run.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+source ${PACER_BINARY_PATH}/setup.sh
+# clean up worksapce
+git clean -dfx .
+# re-parse '.in' files
+../../setup-tests.sh *.in
+../../../BUILD/Moby/moby-driver -r -p=${PACER_SIMULATOR_PATH}/libPacerMobyPlugin.so -s=0.001 model.xml > out.log
+
---
+++ grasp/start/run_all_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source ${BUILD_PATH}/Pacer/setup.sh
+# clean up worksapce
+  
+git clean -dfx . 
+
+# re-parse '.in' files
+${PACER_SCRIPT_PATH}/setup-tests.sh ./*.in
+
+for i in $@
+do
+  git clean -dfx $i
+  ${PACER_SCRIPT_PATH}/setup-tests.sh $i/*.in
+  sed -i.bak "s#>.*</task-directory>#>$i</task-directory>#g" plugins.xml
+  screen -d -m -L ./run.sh
+  sleep 10s
+done
