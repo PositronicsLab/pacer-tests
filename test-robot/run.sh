dbec44ecf511680af60fa15ffc1d42967e03dc72
Samuel Zapolsky <samzapo@gmail.com>
2016-08-18 12:58:22 -0400

    test-robot task (run script for use options)

--- test-robot/run.sh
+++ test-robot/run.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-if [[ $# -ne 2 ]] ; then \
-  echo "2 arguments are needed, run: ${BASH_SOURCE[0]} <path/to/moby-driver> <path/to/robot/model.xml>"; \
+if [[ $# -ne 3 ]] ; then \
+  echo "2 arguments are needed, run: ${BASH_SOURCE[0]} <path/to/moby-driver> <path/to/robot/model.xml> <running velocity>"; \
   exit
 fi
 
@@ -9,6 +9,7 @@ ABS_PATH=$( cd "$( dirname "$2" )" && pwd )
 ROBOT_NAME=$( basename "$2" )
 
 echo "Using robot: $ROBOT_NAME at path: $ABS_PATH ($2)"
+echo "Running at speed: $3"
 
 source ${PACER_BINARY_PATH}/setup.sh
 
@@ -18,7 +19,9 @@ git clean -dfx .
 # re-parse '.in' files
 ${PACER_SCRIPT_PATH}/setup-tests.sh *.in
 
+sed -i.bak "s#>.*</max-forward-speed>#>$3</max-forward-speed>#g" plugins.xml
 sed -i.bak "s#>.*</robot-model>#>$ABS_PATH/$ROBOT_NAME</robot-model>#g" vars.xml
+sed -i.bak "s#>.*</xd>#>$3 0 0.1 0 0 0</xd>#g" vars.xml
 
 $1 -r -p=${PACER_SIMULATOR_PATH}/libPacerMobyPlugin.so -s=0.001 "$ABS_PATH/$ROBOT_NAME"
 
