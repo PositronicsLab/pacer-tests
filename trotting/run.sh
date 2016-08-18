5942c004bfbf2fd69cbc61a42d9635a1cdeb94d0
Samuel Zapolsky <samzapo@gmail.com>
2016-08-18 12:49:28 -0400

    added run script for testing a new robot model

--- trotting/run.sh
+++ trotting/run.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
+if [[ $# -ne 2 ]] ; then \
+  echo "2 arguments are needed, run: ${BASH_SOURCE[0]} <path/to/moby-driver> <path/to/robot/model.xml>"; \
+  exit
+fi
+
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+ABS_PATH=$( cd "$( dirname "$2" )" && pwd )
+ROBOT_NAME=$( basename "$2" )
+
+echo "Using robot: $ROBOT_NAME at path: $ABS_PATH ($2)"
 
 source ${PACER_BINARY_PATH}/setup.sh
+
 # clean up worksapce
 git clean -dfx .
+
 # re-parse '.in' files
 ${PACER_SCRIPT_PATH}/setup-tests.sh *.in
 
-$@ -p=${PACER_SIMULATOR_PATH}/libPacerMobyPlugin.so -s=0.001 model.xml
+sed -i.bak "s#>.*</robot-model>#>$ABS_PATH/$ROBOT_NAME</robot-model>#g" vars.xml
+
+$1 -r -p=${PACER_SIMULATOR_PATH}/libPacerMobyPlugin.so -s=0.001 "$ABS_PATH/$ROBOT_NAME"
 
