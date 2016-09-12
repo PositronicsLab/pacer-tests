292c52b60cc4938213c80fcc558361a9d0d2deec
Samuel Zapolsky <samzapo@gmail.com>
2016-09-12 12:14:52 -0400

    xml-traj uses sample.bin again

--- BotBuilder/XML-traj/run.sh
+++ BotBuilder/XML-traj/run.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 
- ${PACER_COMPONENT_PATH}/monte-carlo-simulation/sample.bin --no-pipe --duration 100 --sample 1 --kinematic
+cp -f $BUILDER_XML_PATH/model-1.xml ./
+sed -i.bak "/\b\(RecurrentForce\)\b/d" ./model-1.xml
+
+
+${PACER_COMPONENT_PATH}/monte-carlo-simulation/sample.bin --no-pipe --float --duration 100 --sample 1 --moby s=0.001 p=${PACER_SIMULATOR_PATH}/libPacerMobyPlugin.so model-1.xml
 
