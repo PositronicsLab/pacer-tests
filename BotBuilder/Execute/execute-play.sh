c0cbcfa3f9de05493ad3c056cd2f9bca521d52ea
Samuel Zapolsky <samzapo@gmail.com>
2017-01-30 16:01:34 -0500

    added 'Execute' testing dir

--- BotBuilder/Execute/execute-play.sh
+++ BotBuilder/Execute/execute-play.sh
@@ -4,7 +4,7 @@
 #the environment variables exported here can be changed except for curr_line and curr_iter
 #unit_ values are the values by which each variable can be incremented by
 #the velocity values affect the speed of the tests
-export name=$2
+export POSE_PATH=$2
 export max_vel=0.5
 export delta_v=0.1
 export curr_vel=0
@@ -20,7 +20,14 @@ ${PACER_SCRIPT_PATH}/setup-tests.sh *.in;
 
 cp $1 ./;
 
-export POSE_PATH=/home/brad/Desktop/pacer-tests/BotBuilder/PoseSets
+line1=$(awk 'NR==1' $2)
+arr=($(echo ${line1}))
+echo "${line1}"
+echo "${arr}"
+echo "${arr[1]}"
+sed -i "16s/\(<q type=\"double\">\)\([^<]*\)\(<[^>]*\)/\1"${arr[0]}" "${arr[3]}" "${arr[6]}" "${arr[9]}" "${arr[12]}" "${arr[15]}" "${arr[18]}" "${arr[21]}" "${arr[24]}" "${arr[27]}" "${arr[30]}" "${arr[33]}"\3/" vars.xml
+sed -i "17s/\(<qd type=\"double\">\)\([^<]*\)\(<[^>]*\)/\1"${arr[1]}" "${arr[4]}" "${arr[7]}" "${arr[10]}" "${arr[13]}" "${arr[16]}" "${arr[19]}" "${arr[22]}" "${arr[25]}" "${arr[28]}" "${arr[31]}" "${arr[34]}"\3/" vars.xml
+sed -i "11s/\(<xd type=\"double\">\)\([^<]*\)\(<[^>]*\)/\1"${arr[36]}" "${arr[37]}" "${arr[38]}" "${arr[39]}" "${arr[40]}" "${arr[41]}"\3/" vars.xml
 
 ${PACER_COMPONENT_PATH}/monte-carlo-simulation/sample.bin --no-pipe --duration 100000000 --moby r s=0.001 p=${PACER_SIMULATOR_PATH}/libPacerMobyPlugin.so model-1.xml --sample 1 --stand
 
